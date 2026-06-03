61d2f7b16b869a7909898b19fbf17edd19649594
DishaOFI <Disha01423@m>
2026-06-03 23:34:42 +0530

    Initial React Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Free Text Requisitions Dashboard</h1>
+      <p>React application is working successfully.</p>
+    </div>
+  );
+}
+
+export default App;
